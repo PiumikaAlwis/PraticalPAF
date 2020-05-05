b1bf9fc0d4ed9dd0c0ad705a2cc1801916f69cf6
PiumikaAlwis <hasini.alwis96@gmail.com>
2020-05-06 02:22:44 +0530

    hospital.js file updated

--- PAF-HelthCare/WebContent/Content/hospital.js
+++ PAF-HelthCare/WebContent/Content/hospital.js
@@ -23,21 +23,70 @@ if (status != true) {
 }
 
 // If valid------------------------
-$("#formItem").submit();
+var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT";
+
+$.ajax({
+	url : "HospitalAPI",
+	type : type,
+	data : $("#formHospital").serialize(),
+	dataType : "text",
+	complete : function(response, status) {
+		onItemSaveComplete(response.responseText, status);
+	}
+});
+
+function onHospitalSaveComplete(response, status)
+{
+	if (status == "success")
+	{
+		var resultSet = JSON.parse(response);
+		
+		if (resultSet.status.trim() == "success")
+		{
+			$("#alertSuccess").text("Successfully saved.");
+			$("#alertSuccess").show();
+			$("#divItemsGrid").html(resultSet.data);
+		} 
+		else if (resultSet.status.trim() == "error"){
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+		} 
+		else if (status == "error"){
+			$("#alertError").text("Error while saving.");
+			$("#alertError").show();
+		} 
+		else{
+			$("#alertError").text("Unknown error while saving..");
+			$("#alertError").show();
+		}
+	
+		$("#hidIDSave").val("");
+		$("#formHospital")[0].reset(); 
+}
 
 // UPDATE==========================================
-$(document).on(
-		"click",
-		".btnUpdate",
-		function(event) {
-			$("#hidIDSave").val(
-					$(this).closest("tr").find('#hidIDUpdate').val());
-			$("#hospitalname").val($(this).closest("tr").find('td:eq(0)').text());
-			$("#address").val($(this).closest("tr").find('td:eq(1)').text());
-			$("#hotline").val($(this).closest("tr").find('td:eq(2)').text());
-			$("#contact").val($(this).closest("tr").find('td:eq(3)').text());
-			$("#dis").val($(this).closest("tr").find('td:eq(3)').text());
-		});
+$(document).on("click", ".btnUpdate", function(event) {
+	$("#hidIDSave").val($(this).closest("tr").find('#hidIDUpdate').val());
+	$("#hospitalname").val($(this).closest("tr").find('td:eq(0)').text());
+	$("#address").val($(this).closest("tr").find('td:eq(1)').text());
+	$("#hotline").val($(this).closest("tr").find('td:eq(2)').text());
+	$("#contact").val($(this).closest("tr").find('td:eq(3)').text());
+	$("#dis").val($(this).closest("tr").find('td:eq(3)').text());
+});
+
+//REMOVE=============================================
+$(document).on("click", "_btnRemove",function(event){
+	$.ajax({
+		url : "HospitalAPI",
+		type : "DELETE",
+		data : "hos_Id"+ $(this).data("hos_Id"),
+		dataType : "text",
+		complete : function(response, status) {
+			onHospitalSaveComplete(response.responseText, status);
+		}
+	});
+});
 
 // CLIENT-MODEL================================================================
 function validateHospitalForm() {
@@ -53,22 +102,22 @@ function validateHospitalForm() {
 	if ($("#hotlien").val().trim() == "") {
 		return "Insert Emergency contact.";
 	}
-	/*// is numerical value
-	var tmpPrice = $("#itemPrice").val().trim();
-	if (!$.isNumeric(tmpPrice)) {
-		return "Insert a numerical value for Item Price.";
-	}
-	// convert to decimal price
-	$("#itemPrice").val(parseFloat(tmpPrice).toFixed(2));*/
-	
-	// CONTACT NUMBER 
+	/*
+	 * // is numerical value var tmpPrice = $("#itemPrice").val().trim(); if
+	 * (!$.isNumeric(tmpPrice)) { return "Insert a numerical value for Item
+	 * Price."; } // convert to decimal price
+	 * $("#itemPrice").val(parseFloat(tmpPrice).toFixed(2));
+	 */
+
+	// CONTACT NUMBER
 	if ($("#contact").val().trim() == "") {
 		return "Insert Regular contact number.";
 	}
-	
+
 	// DESCRIPTION------------------------
 	if ($("#dis").val().trim() == "") {
 		return "Insert Description.";
 	}
 	return true;
 }
+
